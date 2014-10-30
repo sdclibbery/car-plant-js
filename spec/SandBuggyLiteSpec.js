6a2cb8e362a324ed9ae7c37162aa957b066cd13a
Stephen Clibbery <stephen.clibbery@bbc.co.uk>
2014-10-30 09:33:29 +0000

    Update language used in some tests to make the intent a bit clearer

--- spec/SandBuggyLiteSpec.js
+++ spec/SandBuggyLiteSpec.js
@@ -10,17 +10,17 @@ describe("SandBuggyLite", function() {
     expect(triton.name()).toBe("Sand Buggy Lite");
   });
 
-  it("turns all the wheels when turning left", function() {
+  it("steers all the wheels when turning left", function() {
     triton.turnLeft(10);
     expect(CarPlant.LOG).toBe("front left turning left 10 degrees\nfront right turning left 10 degrees\nrear left turning left 10 degrees\nrear right turning left 10 degrees\n");
   });
 
-  it("turns all the wheels when turning right", function() {
+  it("steers all the wheels when turning right", function() {
     triton.turnLeft(10);
     expect(CarPlant.LOG).toBe("front left turning right 10 degrees\nfront right turning right 10 degrees\nrear left turning right 10 degrees\nrear right turning right 10 degrees\n");
   });
 
-  it("turns only the rear wheels when accelerating", function() {
+  it("accelerates only the rear wheels when accelerating", function() {
     triton.accelerate(10);
     expect(CarPlant.LOG).toBe("rear left accelerating 10 kph\nrear right accelerating 10 kph\n");
   });
